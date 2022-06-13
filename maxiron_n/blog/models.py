a4d786fd7805e82f4fe0c3b01591adee74ebd06f
Maxiron <43835502+Maxiron@users.noreply.github.com>
2022-06-13 05:02:34 +0100

    first commit

---
+++ maxiron_n/blog/models.py
@@ -0,0 +1,11 @@
+from tkinter import CASCADE
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250) 
+    text = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    Created_date = models.DateTimeField()
+    Published_date = models.DateTimeField()
