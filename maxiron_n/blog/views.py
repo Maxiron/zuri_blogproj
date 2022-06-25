d7b7632e2d77d47e56291daab7d14366d7c1c149
Maxiron <43835502+Maxiron@users.noreply.github.com>
2022-06-25 12:48:14 +0100

    blog_crud

--- maxiron_n/blog/views.py
+++ maxiron_n/blog/views.py
@@ -1,3 +1,33 @@
+from msilib.schema import ListView
+from pyexpat import model
+# from attr import fields
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from .models import Post
+from django.views.generic import ListView, CreateView, UpdateView, DetailView
 
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
